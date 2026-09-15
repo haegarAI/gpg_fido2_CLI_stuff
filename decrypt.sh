0fbfb509ff90ffa2e2feff31ab1acfaa43445542
Stefan <stefan.a.essl-pokorny@a1.at>
2026-09-15 22:25:04 +0200

    Update decrypt.sh with new version echo and gpg command
    Added a new version echo statement and updated the gpg command to use fido2-cred for passphrase retrieval.

--- decrypt.sh
+++ decrypt.sh
@@ -4,6 +4,10 @@
 ### '4711' is a hardcoded dummy value and has no effect, it could be anything...
 ### took the 4th line of the response code from FIDO2 key (hardcoded dev /dev/hidraw4) and there char 1-44 as passphrase
 echo "prepare your key and don't forget to push the button when it is blinking!"
+echo "new version:"
+gpg --passphrase "$(fido2-cred -M -h /dev/hidraw4 < <(echo -e "$(echo '4711' | openssl sha256 -binary |  base64)\n"$2"\n4711\n4711)" )|sed -n "4p;" |cut -c 1-44 )" --pinentry-mode loopback -d $1
+
+echo "first version:"
 xargs -n1 bash -c 'gpg --passphrase "$((fido2-cred -M -h /dev/hidraw4 <<<"$(echo "4711" | openssl sha256 -binary |  base64)
 $0
 4711
